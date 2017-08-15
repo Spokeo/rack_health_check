dc4aa8dac63be3ac86086c9306c9fe58dbf73df9
Mike Ballou <mballou@spokeo.com>
2017-08-15 10:29:42 -0700

    Moved the middleware to be the first middleware

--- lib/rack_health_check/railtie.rb
+++ lib/rack_health_check/railtie.rb
@@ -1,7 +1,7 @@
 module RackHealthCheck
   class Railtie < Rails::Railtie
     initializer "rack_health_check.configure_rails_initialization" do
-      Rails.application.middleware.use RackHealthCheck::Middleware
+      Rails.application.middleware.insert_before 0, RackHealthCheck::Middleware
     end
   end
 end
